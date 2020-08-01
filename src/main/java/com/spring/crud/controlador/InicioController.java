e38a70de22ac42bfd045e5490527eb4f6af5c1d2
Marcos Lopez <lgmarcosxavier@gmail.com>
2020-08-01 00:09:51 -0600

    mejora comentarios, application propoerties

--- src/main/java/com/spring/crud/controlador/InicioController.java
+++ src/main/java/com/spring/crud/controlador/InicioController.java
@@ -3,10 +3,11 @@ package com.spring.crud.controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // para marcarla como clae MVC y sea cargada en el contenedor de Spring
 public class InicioController {
-	@GetMapping("/")
+
+	@GetMapping("/") // ruta mediante el método get
 	public String getInicio() {
-		return "index";
+		return "index"; // devuelve el archivo "index.html" de los recursos template
 	}
 }
